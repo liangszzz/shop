83b51ac5e5109fd07e248c20e505fa71a4bf6040
liang <liang-shan@outlook.com>
2019-11-18 16:53:41 +0800

    code

--- stream-service/src/main/java/com/github/ls/stream/service/OrderMqService.java
+++ stream-service/src/main/java/com/github/ls/stream/service/OrderMqService.java
@@ -5,8 +5,12 @@ import com.github.ls.common.entity.ResponseData;
 import com.github.ls.common.order.mq.AddAttachmentVO;
 import com.github.ls.stream.channel.UploadAttachmentChannel;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +21,22 @@ public class OrderMqService {
 
     private final UploadAttachmentChannel uploadAttachmentChannel;
 
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
     public OrderMqService(UploadAttachmentChannel uploadAttachmentChannel) {
         this.uploadAttachmentChannel = uploadAttachmentChannel;
     }
 
     public ResponseData attachmentUploadMq(AddAttachmentVO vo) {
-        boolean send = uploadAttachmentChannel.output().send(MessageBuilder.withPayload(vo).build());
-        return new ResponseData(send ? ResponseCode.SUCCESS : ResponseCode.SERVER_EXCEPTION);
+        log.info("接收:" + vo.toString());
+
+        Message<AddAttachmentVO> message = MessageBuilder.withPayload(vo).setHeader("DELAY", 4).build();
+        boolean send = uploadAttachmentChannel.output().send(message,2000);
+
+//        SendResult sendResult = rocketMQTemplate.syncSend("upload-attachment-topic", MessageBuilder.withPayload(vo).build(), 2000, 4);
+//        log.info(sendResult.toString());
+        return new ResponseData(true ? ResponseCode.SUCCESS : ResponseCode.SERVER_EXCEPTION);
     }
 
     @StreamListener(UploadAttachmentChannel.INPUT)

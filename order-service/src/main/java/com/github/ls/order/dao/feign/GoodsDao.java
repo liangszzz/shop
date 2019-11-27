7fec3d370157f1f3b946c15537dbc4585b9f1671
liang <liang-shan@outlook.com>
2019-11-27 15:54:31 +0800

    add api

--- order-service/src/main/java/com/github/ls/order/dao/feign/GoodsDao.java
+++ order-service/src/main/java/com/github/ls/order/dao/feign/GoodsDao.java
@@ -1,7 +1,7 @@
 package com.github.ls.order.dao.feign;
 
 import com.github.ls.common.entity.ResponseData;
-import com.github.ls.coupon.entity.Coupon;
+import com.github.ls.goods.entity.Goods;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +10,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@FeignClient(value = "spring-cloud-coupon")
-public interface CouponDao {
+@FeignClient(value = "spring-cloud-goods")
+public interface GoodsDao {
 
-    @PostMapping("api/v1/coupon/consumer")
-    ResponseData consumerGoods(@NotNull @RequestParam("goods") List<Coupon> coupons,
-                               @NotBlank @RequestParam("order_no") String orderNo);
+    @PostMapping("api/v1/goods/consumer")
+    ResponseData consumer(@NotNull @RequestParam("goods") List<Goods> goods, @NotBlank @RequestParam("order_no") String orderNo);
 
 }
